6026ed077ce30689a597842d0417356237463f70
Sid-Turner-Ellis <sid.turner.ellis@gmail.com>
2023-12-02 10:01:26 +0700

    feat(web): adds badge

---
+++ apps/web/src/components/badge.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/utils/cn";
+import { VariantProps, cva } from "class-variance-authority";
+import { ClassValue } from "clsx";
+import { ReactNode } from "react";
+
+const badgeVariants = cva(
+  "px-[6px] py-[1px] inline-block text-sm rounded-[7px]",
+  {
+    variants: {
+      colorScheme: {
+        emerald: "bg-emerald-100 text-emerald-950",
+        rose: "bg-rose-100 text-rose-950",
+      },
+    },
+  }
+);
+
+export const Badge = ({
+  children,
+  colorScheme,
+  className,
+}: { children: ReactNode; className?: ClassValue } & VariantProps<
+  typeof badgeVariants
+>) => (
+  <div className={cn(badgeVariants({ colorScheme }), className)}>
+    <p className="font-medium">{children}</p>
+  </div>
+);
