07282e3393bc1cad1ff1d0ed9d7635bbb97a2e1c
Sid-Turner-Ellis <sid.turner.ellis@gmail.com>
2024-01-30 16:26:05 +0700

    chore(web): remove RQDT

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { TokenProvider } from "@/providers/token-provider";
 import React from "react";
 import { ConditionallyWrap } from "@/components/conditionally-wrap";
 import { RegistrationModalProvider } from "@/providers/registration-modal-provider";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastProvider } from "@/providers/toast-provider";
 import { ErrorBoundary } from "@/components/error-boundary";
 import { Inter, Bricolage_Grotesque } from "@next/font/google";
@@ -23,7 +23,7 @@ const grotesque = Bricolage_Grotesque({
   subsets: ["latin"],
   variable: "--font-grotesque",
 });
-
+//
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={cn("w-full h-full", inter.variable, grotesque.variable)}>
@@ -36,9 +36,9 @@ export default function App({ Component, pageProps }: AppProps) {
                   condition={!pageProps.hideSidebar}
                   Wrapper={SiteLayout}
                 >
-                  {process.env.NODE_ENV === "development" && (
+                  {/* {process.env.NODE_ENV === "development" && (
                     <ReactQueryDevtools />
-                  )}
+                  )} */}
                   <RegistrationModalProvider>
                     <ErrorBoundary>
                       <Component {...pageProps} />
