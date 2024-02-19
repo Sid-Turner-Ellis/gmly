24a08b9b7d8564daa34332f56068c3e7c47bf923
Sid-Turner-Ellis <sid.turner.ellis@gmail.com>
2024-02-19 21:13:18 +0700

    feat(api): populate balances for teams and team profiles

--- apps/api/src/api/team-profile/services/team-profile.ts
+++ apps/api/src/api/team-profile/services/team-profile.ts
@@ -6,6 +6,44 @@ import { factories } from "@strapi/strapi";
 import merge from "deepmerge";
 
 export default factories.createCoreService("api::team-profile.team-profile", {
+  async findOne(entityId, params) {
+    const teamProfile = await super.findOne(entityId, params);
+    const profile = teamProfile?.profile;
+
+    if (profile?.id) {
+      profile.balance = await strapi
+        .service("api::profile.profile")
+        .getBalanceForProfile(profile.id);
+    }
+
+    return teamProfile;
+  },
+
+  async find(params) {
+    const teamProfiles = await super.find(params);
+    const profileIds =
+      teamProfiles.results
+        ?.map((teamProfile) => teamProfile.profile?.id)
+        .filter(Boolean) ?? [];
+
+    if (profileIds.length) {
+      const profileBalances = await strapi
+        .service("api::profile.profile")
+        .getBalanceForProfiles(profileIds);
+
+      teamProfiles.results.forEach((teamProfile) => {
+        if (teamProfile.profile) {
+          teamProfile.profile.balance =
+            profileBalances.find(
+              (profile) => profile.id === teamProfile.profile.id,
+            )?.balance ?? 0;
+        }
+      });
+    }
+
+    return teamProfiles;
+  },
+
   async findTeamProfilesByTeamId(teamId, params: any = {}) {
     const mergedParams = merge(params, {
       filters: {
--- apps/api/src/api/team/services/team.ts
+++ apps/api/src/api/team/services/team.ts
@@ -5,16 +5,56 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreService("api::team.team", {
-  //   getProfileByWalletAddress: async (walletAddress) => {
-  //     if (!walletAddress) {
-  //       return null;
-  //     }
-  //     const { results } = await strapi
-  //       .service("api::profile.profile")
-  //       .find({ filters: { wallet_address: walletAddress } });
-  //     const { results: profileResults } = await strapi
-  //       .service("api::profile.profile")
-  //       .find({ filters: { wallet_address: walletAddress } });
-  //     const profile = profileResults[0];
-  //   },
+  async findOne(entityId, params) {
+    const team = await super.findOne(entityId, params);
+    const profileIds = team?.team_profiles
+      .map((teamProfile) => teamProfile.profile?.id)
+      .filter(Boolean);
+
+    if (profileIds.length > 0) {
+      const profileBalances = await strapi
+        .service("api::profile.profile")
+        .getBalanceForProfiles(profileIds);
+
+      team?.team_profiles.forEach((teamProfile) => {
+        if (teamProfile.profile) {
+          teamProfile.profile.balance = profileBalances.find(
+            (profile) => profile.id === teamProfile.profile.id,
+          )?.balance;
+        }
+      });
+    }
+
+    return team;
+  },
+
+  async find(params) {
+    const teams = await super.find(params);
+    const profileIds = teams.results
+      ?.map(
+        (team) =>
+          team?.team_profiles.map((teamProfile) => teamProfile.profile?.id),
+      )
+      .flat()
+      .filter(Boolean);
+
+    if (profileIds.length > 0) {
+      const profileBalances = await strapi
+        .service("api::profile.profile")
+        .getBalanceForProfiles(profileIds);
+
+      teams.results.forEach((team) => {
+        team.team_profiles?.forEach((teamProfile) => {
+          if (teamProfile.profile) {
+            teamProfile.profile.balance =
+              profileBalances.find(
+                (profile) => profile.id === teamProfile.profile.id,
+              )?.balance ?? 0;
+          }
+        });
+      });
+    }
+
+    return teams;
+  },
 });
